398f9237307bd21e0ba1ff741f749a2f43f69f6b
João Carlos Ressel <joao.resselti@gmail.com>
2024-11-06 13:11:41 -0500

    iniciando a criação do modal

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,14 +1,22 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
-import logo from '../../img/logo.png';
+import logo from "../../img/logo.png";
+
+import * as Dialog from "@radix-ui/react-dialog";
+import { NewTransactionsModal } from "../../pages/transactions/components/new-transactions-modal";
 
 export const Header = () => {
   return (
-
     <HeaderContainer>
-        <HeaderContent>
-            <img src={logo} alt="" />
+      <HeaderContent>
+        <img src={logo} alt="" />
+
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
             <NewTransactionButton>Nova transação</NewTransactionButton>
-        </HeaderContent>
+          </Dialog.Trigger>
+          <NewTransactionsModal/>
+        </Dialog.Root>
+      </HeaderContent>
     </HeaderContainer>
   );
 };
